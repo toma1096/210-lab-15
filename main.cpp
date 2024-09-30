2af5fb5296033f0a5095a4111c2d285d3fedce2c
realestestate <>
2024-09-29 20:43:12 -0700

    fills out movie class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -11,35 +12,19 @@ class Movie{
     public:
     void setYear(int Year){year=Year;}
     void setTitle(string Title){title = Title;}
-    void setWriter()
+    void setWriter(string Writer){writer = Writer}
     //getters
 
     //constructor
-    Movie(int Year, string Title){setYear(Year);setTitle(Title));}
-    void printColor(){
-        cout<<"R: "<<r<<endl;
-        cout<<"G: "<<g<<endl;
-        cout<<"B: "<<b<<endl;
-        cout<<"====================\n";
+    Movie(int Year, string Title,string Writer){setYear(Year);setTitle(Title);setWriter(Writer);}
+    void printMovie(){
+        cout<<"Movie: "<<title<<endl;
+        cout<<setw(5)<<"Year Released: "<<year<<endl;
+        cout<<"Screenwriter: "<<writer<<endl;
+        cout<<endl;
     }
 };
 
 int main(){
-    Color *C1 = new Color(1,2,3);
-    Color *C2 = new Color(100,222,30);
-    Color *C3 = new Color(29,80,240);
-    Color *C4 = new Color(190,5,77);
-    cout<<"Color 1\n";
-    C1->printColor();
-    cout<<"Color 2\n";
-    C2->printColor();
-    cout<<"Color 3\n";
-    C3->printColor();
-    cout<<"Color 4\n";
-    C4->printColor();
-    delete C1;
-    delete C2;
-    delete C3;
-    delete C4;
-    return 0;
+    
 }
